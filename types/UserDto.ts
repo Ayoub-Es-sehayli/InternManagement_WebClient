ae7f256064ee379d36dfa1ca0dd5adba929350e7
wiamES <wiam.essaadi.20@gmail.com>
2021-07-15 14:28:17 +0100

    Users Management UI

---
+++ types/UserDto.ts
@@ -0,0 +1,7 @@
+type UserDto ={
+    id: Number
+    nom_complet: string
+    email: string 
+}
+
+export default UserDto
---
+++ types/UserModel.ts
@@ -0,0 +1,9 @@
+type UserModel = {
+    nom: string
+    prenom: string
+    email: string
+    role: number
+    password: string
+}
+
+export default UserModel
